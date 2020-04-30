9c68912ff13dbcfc4d1012e69a7061bb3083a3db
Joann Pan <joannlpan@gmail.com>
2020-04-30 23:43:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,7 +9,13 @@ def countdown(seconds_to_midnight)
 end
 
 
-def countdown_with_sleep
-
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0 
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+    sleep(1)
+    end
+    "HAPPY NEW YEAR!"
+    end
 
 end
